8025cf44ed62c44327dd29872315f02133819b49
Tento <regartento@gmail.com>
2026-02-15 09:55:24 +0700

    add return keyword

--- mini_saas_api/src/routes/mod.rs
+++ mini_saas_api/src/routes/mod.rs
@@ -3,7 +3,7 @@ use axum::{routing::get, Router};
 use crate::handlers::health_handler;
 
 pub fn create_routes() -> Router {
-    Router::new()
+    return Router::new()
         .route("/", get(health_handler::root))
-        .route("/health", get(health_handler::health))
+        .route("/health", get(health_handler::health));
 }
